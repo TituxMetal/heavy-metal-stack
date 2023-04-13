7bec163dce5fec4ad42dd364975a407e76bc2213
Titux Metal (MBP) <github@lgdweb.fr>
2023-04-13 04:40:56 +0200

    Formated and linted all project files

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,42 @@
-import type { LinksFunction, LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { LinksFunction, LoaderArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
 
-import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
+import { getUser } from './session.server'
+import tailwindStylesheetUrl from './styles/tailwind.css'
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
+  return [{ rel: 'stylesheet', href: tailwindStylesheetUrl }]
+}
 
 export async function loader({ request }: LoaderArgs) {
   return json({
-    user: await getUser(request),
-  });
+    user: await getUser(request)
+  })
 }
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang='en' className='h-full'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body className='h-full'>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
