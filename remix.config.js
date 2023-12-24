7997fe0fac0378d7b609cec1a2cce5e931732080
Titux Metal (DEV) <github@lgdweb.fr>
2023-12-24 15:07:50 +0100

    Update remix.config.js with new server module format Remove all v2 future flags Enable tailwind and postcss

--- remix.config.js
+++ remix.config.js
@@ -5,14 +5,9 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*'],
   appDirectory: 'src',
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    unstable_tailwind: true,
-    unstable_postcss: true
-  },
+  serverModuleFormat: 'cjs',
+  tailwind: true,
+  postcss: true,
   routes: async defineRoutes => {
     return flatRoutes('routes', defineRoutes, { appDir: 'src' })
   }
