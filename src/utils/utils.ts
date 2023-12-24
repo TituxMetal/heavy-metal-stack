032a607a76d1f91511f2992b28190f3e1daead50
Titux Metal (DEV) <github@lgdweb.fr>
2023-12-24 15:11:39 +0100

    Refactor useMatchesData and isUser functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -41,11 +41,16 @@ export const useMatchesData = (
     () => matchingRoutes.find(route => route.id === id),
     [matchingRoutes, id]
   )
-  return route?.data
+  return route?.data as Record<string, unknown>
 }
 
-const isUser = (user: any): user is User => {
-  return user && typeof user === 'object' && typeof user.email === 'string'
+const isUser = (user: unknown): user is User => {
+  return (
+    user != null &&
+    typeof user === 'object' &&
+    'email' in user &&
+    typeof user.email === 'string'
+  )
 }
 
 export const useOptionalUser = (): User | undefined => {
